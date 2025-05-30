b11bf40aaabea1c795a77684b69450ce61fb49ff
Alice <alicegillbanks@gmail.com>
2025-05-30 19:48:52 +0100

    Initial commit

---
+++ src/pages/Longform.jsx
@@ -0,0 +1,9 @@
+function Longform() {
+    return (
+        <div>
+            <h1>this is a header</h1>
+            <p>this is a paragraph</p>
+        </div> 
+    )
+}
+export default Longform;
