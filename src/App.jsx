7fb050dc32ea0ff1f7269f174a0953c0d92c0820
Alice <alicegillbanks@gmail.com>
2025-06-10 22:08:01 +0100

    Major improvements, first query success

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route, Link} from 'react-router-dom';
-import Longform from '.pages/longform';
-import MulChoice from '.pages/mulChoice';
+import { Routes, Route, Link } from 'react-router-dom';
+import Longform from './pages/longform.jsx';
+import MulChoice from './pages/mulChoice.jsx';
 
 function App() {
   return (
     <div>
       <nav>
-        <link to='/longform'>Longform Questions</link> | <link to='/mulchoice'>Multiple Choice</link>
+        <Link to='/longform'>Longform Questions</Link> | <Link to='/mulchoice'>Multiple Choice</Link>
       </nav>
 
       <Routes>
